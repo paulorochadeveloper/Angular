4bd8ab8428adaa1e3b7824c796f12ca010252e77
Paulo Rocha <paulorochaweb@bitbucket.org>
2019-09-23 17:42:24 +0100

    #MODULO 3 CONSUMINDO API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
 import { PhotosModule } from './photos/photos.module';
+
 
 @NgModule({
   declarations: [
