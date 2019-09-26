61ea374ac85810a1d51650aa88ff7f57c8e74dea
Paulo Rocha <paulorochaweb@bitbucket.org>
2019-09-26 17:08:10 +0100

    #FINISH CURSO ANGULAR1 - DIRETIVAS

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -29,20 +28,19 @@ export class PhotoListComponent implements OnInit {
     ngOnInit(): void {
       this.userName = this.activatedRoute.snapshot.params.userName;
       this.photos = this.activatedRoute.snapshot.data['photos'];
-      this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
+
     }
 
     ngOnDestroy(): void {
-        this.debounce.unsubscribe();
+        
     }
     load() {
       this.photoService
           .listFromUserPaginated(this.userName, ++this.currentPage)
           .subscribe(photos => {
+              this.filter = '';
               this.photos = this.photos.concat(photos);
               if(!photos.length) this.hasMore = false;
           });
-  }
+   }
 }
